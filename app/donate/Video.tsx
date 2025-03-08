757ac3507631d551571f453aa3c184035b53a4ed
Nolger Rodriguez <nolgerluisrodriguezmedina@gmail.com>
2025-03-08 15:14:08 -0500

    Update Video component to use PayPal link and local video source

--- app/donate/Video.tsx
+++ app/donate/Video.tsx
@@ -4,8 +4,8 @@ export default function Video() {
       {/* Video de YouTube */}
       <div className="w-full max-w-lg">
         <iframe 
-          className="w-full h-60 md:h-80 rounded-lg"
-          src="https://www.youtube.com/watch?v=Y2hTxA3-CLs" 
+          className="w-full h-50 md:h-80 rounded-lg"
+          src="/videos/paypal.mp4" 
           title="Cómo donar en Patreon"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
@@ -15,7 +15,7 @@ export default function Video() {
 
       {/* Botón de Donación */}
       <a
-        href="https://www.patreon.com" // Reemplaza con el enlace real
+        href="https://www.paypal.com" // Reemplaza con el enlace real
         target="_blank"
         rel="noopener noreferrer"
         className="bg-yellow-500 text-white px-6 py-2 rounded-lg font-semibold hover:bg-yellow-600"
